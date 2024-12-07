60e8d00045f87d322f91b935190dc3367489e9b9
Harsh <mishraharsh.2803@gmail.com>
2024-12-07 18:29:45 +0530

    helloo

---
+++ main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int array = sc.nextInt();
+
+        for ( int i = 0 ; <= array ; i++){
+
+        }
+        for ( int j = 0 ; j<= array ; j++){
+
+        }
+        int array 
+
+
+        
+        
+
+    }
+}
